20ddab6b69271b9ad5d11367af4110f313365b90
anuragKumarCB <anuragkumar.wmail@gmail.com>
2026-08-30 14:22:54 +0530

    Hello World Program

---
+++ 00 HelloWorld/src/main/java/io/github/anurag/Main.java
@@ -0,0 +1,18 @@
+package io.github.anurag;
+// this is a package if you enable package then your program should contain the package name
+
+public class Main {
+//    this is main class everything in java is class
+
+    public static void main(String[] args){
+//        this is main method or function every java program execute the main method
+        String greeting = "Hello Bot, memorise it!";
+//        string in java is not a data type but a class
+        System.out.println(greeting);
+
+//        print start for each character underneath
+        for (int i = 0; i < greeting.length(); i++) {
+            System.out.print("*");
+        }
+    }
+}
